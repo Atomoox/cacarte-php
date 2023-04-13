c0403b0d56435c5e19998240da958132ea770963
Atomox_ <aoxresell@gmail.com>
2023-04-13 10:39:46 +0200

    Custom reactive

--- ressources/js/react.js
+++ ressources/js/react.js
@@ -1,1 +1,94 @@
-let l=new WeakMap;let c=new Map;let o=null;function f(t){o=t;t();o=null}function a(t,e){if(l.get(t).get(e)===undefined)l.get(t).set(e,new Set);l.get(t).get(e).add(o)}function r(e,n){if(l.get(e).get(n)===undefined)return;for(let t of l.get(e).get(n))t()}function t(t,e){l.set(t,new Map);const n={get(t,e,n){if(o!==null)a(t,e);return Reflect.get(t,e,n)},set(t,e,n,l){const o=Reflect.set(t,e,n,l);r(t,e);return o}};c.set(e,new Proxy(t,n));return c.get(e)}function u(e=document){for(let t of e.querySelectorAll("input.reactiveInput")){const[o,a]=t.dataset.inputvar.split(".");t.addEventListener("change",t=>{c.get(o)[a]=t.target.value})}for(let t of e.querySelectorAll("[data-onclick]")){const[o,r,s]=t.dataset.onclick.split(/[.()]+/);t.addEventListener("click",t=>{c.get(o)[r](s)})}for(let t of e.querySelectorAll("[data-textvar]")){const[o,a]=t.dataset.textvar.split(".");f(()=>{t.textContent=c.get(o)[a]})}for(let t of e.querySelectorAll("[data-textfun]")){const[o,r,s]=t.dataset.textfun.split(/[.()]+/);f(()=>{t.textContent=c.get(o)[r](s)})}for(let t of e.querySelectorAll("[data-stylefun]")){const[o,r,s]=t.dataset.stylefun.split(/[.()]+/);f(()=>{Object.assign(t.style,c.get(o)[r](s))})}const n=e.querySelectorAll("[data-htmlvar]");const l=e.querySelectorAll("[data-htmlfun]");for(let t of n){const[o,a]=t.dataset.htmlvar.split(".");f(()=>{t.innerHTML=c.get(o)[a];u(t)})}for(let t of l){const[o,r,s]=t.dataset.htmlfun.split(/[.()]+/);f(()=>{t.innerHTML=c.get(o)[r](s);u(t)})}}export{f as applyAndRegister,t as reactive,u as startReactiveDom};
+let reactiveData = new WeakMap;
+let reactiveComponent = new Map;
+let currentComponent = null;
+
+function applyAndRegister(componentFunction) {
+    currentComponent = componentFunction;
+    componentFunction();
+    currentComponent = null;
+}
+
+function registerReactiveData(component, prop) {
+    if (reactiveData.get(component).get(prop) === undefined) reactiveData.get(component).set(prop, new Set);
+    reactiveData.get(component).get(prop).add(currentComponent);
+}
+
+function notifyReactiveDataChanges(component, prop) {
+    if (reactiveData.get(component).get(prop) === undefined) return;
+    for (let reactiveComponentFunction of reactiveData.get(component).get(prop)) {
+        reactiveComponentFunction();
+    }
+}
+
+function reactive(componentData, componentName) {
+    reactiveData.set(componentData, new Map);
+    const reactiveProxyHandler = {
+        get(target, prop, receiver) {
+            if (currentComponent !== null) registerReactiveData(target, prop);
+            return Reflect.get(target, prop, receiver);
+        },
+        set(target, prop, value, receiver) {
+            const result = Reflect.set(target, prop, value, receiver);
+            notifyReactiveDataChanges(target, prop);
+            return result;
+        }
+    };
+    reactiveComponent.set(componentName, new Proxy(componentData, reactiveProxyHandler));
+    return reactiveComponent.get(componentName);
+}
+
+function startReactiveDom(element = document) {
+    for (let inputElement of element.querySelectorAll("input.reactiveInput")) {
+        const [componentName, propName] = inputElement.dataset.inputvar.split(".");
+        inputElement.addEventListener("change", event => {
+            reactiveComponent.get(componentName)[propName] = event.target.value;
+        });
+    }
+    for (let clickableElement of element.querySelectorAll("[data-onclick]")) {
+        const [componentName, methodName, methodArg] = clickableElement.dataset.onclick.split(/[.()]+/);
+        clickableElement.addEventListener("click", event => {
+            reactiveComponent.get(componentName)[methodName](methodArg);
+        });
+    }
+    for (let textElement of element.querySelectorAll("[data-textvar]")) {
+        const [componentName, propName] = textElement.dataset.textvar.split(".");
+        applyAndRegister(() => {
+            textElement.textContent = reactiveComponent.get(componentName)[propName];
+        });
+    }
+    for (let textFunctionElement of element.querySelectorAll("[data-textfun]")) {
+        const [componentName, methodName, methodArg] = textFunctionElement.dataset.textfun.split(/[.()]+/);
+        applyAndRegister(() => {
+            textFunctionElement.textContent = reactiveComponent.get(componentName)[methodName](methodArg);
+        });
+    }
+    for (let styleFunctionElement of element.querySelectorAll("[data-stylefun]")) {
+        const [componentName, methodName, methodArg] = styleFunctionElement.dataset.stylefun.split(/[.()]+/);
+        applyAndRegister(() => {
+            Object.assign(styleFunctionElement.style, reactiveComponent.get(componentName)[methodName](methodArg));
+        });
+    }
+    const htmlVarElements = element.querySelectorAll("[data-htmlvar]");
+    const htmlFunElements = element.querySelectorAll("[data-htmlfun]");
+    for (let htmlVarElement of htmlVarElements) {
+        const [componentName, propName] = htmlVarElement.dataset.htmlvar.split(".");
+        applyAndRegister(() => {
+            htmlVarElement.innerHTML = reactiveComponent.get(componentName)[propName];
+            startReactiveDom(htmlVarElement);
+        });
+    }
+    for (let htmlFunElement of htmlFunElements) {
+        const [componentName, methodName, methodArg] = htmlFunElement.dataset.htmlfun.split(/[.()]+/);
+        applyAndRegister(() => {
+            htmlFunElement.innerHTML = reactiveComponent.get(componentName)[methodName](methodArg);
+            startReactiveDom(htmlFunElement);
+        });
+    }
+}
+
+// Exporting the public functions
+export {
+    applyAndRegister,
+    reactive,
+    startReactiveDom,
+};
